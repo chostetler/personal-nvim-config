15c97138da8f606f38b5e035a4e5c51093a3b5c0
Caleb Hostetler <chostetler@emxinc.com>
2025-11-07 11:32:07 -0500

    Add jetpack

--- lua/init.lua
+++ lua/init.lua
@@ -338,3 +338,5 @@ vim.keymap.set('n', '<leader> ', builtin.find_files, { desc = 'Telescope find fi
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.live_grep, { desc = 'Telescope help tags' })
+
+vim.keymap.set("n", "<leader>l", "<cmd>ls<CR>:b ", { desc = "List buffers then :b ..." })
